7a69fea74fd7caf51e8f7f726bf0200dbe3ca0eb
elzaidir <mathieu@bourquenoud.net>
2021-11-14 20:35:02 +0100

    Added simple plugin generation script

--- generate_plugin.sh
+++ generate_plugin.sh
@@ -1,3 +1,12 @@
 #!/usr/bin/bash
 
+echo "********GENERATING PLUGIN*********"
+echo "Removing old plugin"
 
+rm crunchyroll_plugin.zip
+
+echo "Creating new plugin"
+
+zip -r crunchyroll_plugin.zip plugin.video.crunchyroll
+
+echo "DONE!"
